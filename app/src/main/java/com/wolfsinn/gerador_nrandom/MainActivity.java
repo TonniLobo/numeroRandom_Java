4e614046195328f35a9702043c491acbfca188e6
TonniLobo <grohli18@hotmail.com>
2022-03-26 21:16:44 -0300

    first commit

---
+++ app/src/main/java/com/wolfsinn/gerador_nrandom/MainActivity.java
@@ -0,0 +1,24 @@
+package com.wolfsinn.gerador_nrandom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void jogar(View view){
+        TextView textResultado;
+        textResultado = findViewById(R.id.textResultado);
+        int numero = new Random().nextInt(11);
+        textResultado.setText("Resultado "+numero);
+    }
+}
